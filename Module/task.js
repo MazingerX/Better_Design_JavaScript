c02633468932e5be2bc22353b918bd5fda75349c
mazingerx <lehinostroza@icloud.com>
2019-10-18 12:46:17 -0500

    Using Module pattern

---
+++ Module/task.js
@@ -0,0 +1,19 @@
+var Task = function(data) 
+{
+    this.name = data.name;
+    this.completed = false;
+}
+
+Task.prototype.complete = function() 
+{
+    console.log('completing task: ' + this.name);
+     this.completed = true; 
+};
+
+Task.prototype.save = function() 
+{
+    console.log('saving Task: ' + this.name);
+};
+
+
+module.exports = Task;
